20b0f0c8db2b3b1c892af6c8748d450ea932445d
Basnetsuman4 <arunbasnet54@gmail.com>
2025-03-11 14:03:07 +0545

    Using Template via Express

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-	console.log("     [ A new request received at ] : " + Date.now());
-	next();
-});
+
 const PORT = 9000;
 
-app.use("/hawa", (req, res, next) => {
-	console.log("Hawa is here");
-	next();
-});
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.send("Welcome to our API");
-});
-app.get("/hawa", (req, res) => {
-	res.send("Welcome to our API from hawa");
+	const userName = "John Doe";
+	res.render(`index`, { userName });
 });
 
 // Catching all invalid route
